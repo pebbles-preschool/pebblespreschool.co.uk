3ff6d3371fb140620cf501683976a8aa15a739f1
Olly Nevard <olly@pebblespreschool.co.uk>
2019-12-26 22:55:47 +0000

    Remove products pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,6 @@ import cloudinary from 'netlify-cms-media-library-cloudinary';
 import StaffPagePreview from './preview-templates/StaffPagePreview';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
@@ -14,5 +13,4 @@ CMS.registerMediaLibrary(cloudinary);
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('staff', StaffPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
