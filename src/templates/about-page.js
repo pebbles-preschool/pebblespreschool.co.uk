b4140492e56433f89f6ae8f9f1f7506387c8051f
Olly Nevard <olly@pebblespreschool.co.uk>
2019-12-30 00:41:54 +0000

    Update about-us template to use Grommet Header

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
+import { Heading } from 'grommet';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
 
@@ -13,7 +14,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">{title}</h2>
+              <Heading>{title}</Heading>
               <PageContent className="content" content={content} />
             </div>
           </div>
